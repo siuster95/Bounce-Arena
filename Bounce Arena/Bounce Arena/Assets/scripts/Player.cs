df721387c54ae003314f5558a8d29b0a2069aaac
siuster95 <jxs7205@rit.edu>
2017-01-09 19:08:16 -0500

    Obstacle collision with player

--- Bounce Arena/Bounce Arena/Assets/scripts/Player.cs
+++ Bounce Arena/Bounce Arena/Assets/scripts/Player.cs
@@ -23,6 +23,7 @@ public class Player : MonoBehaviour {
     private Vector3 position;
     private Vector3[] coinposition;
     private GameManager GameManager;
+    private Obstacle[] ObstaclesArray;
 
 	// Use this for initialization
 	void Start () {
@@ -37,6 +38,7 @@ public class Player : MonoBehaviour {
         GameObject GameManagerGO = GameObject.Find("GameManager");
         GameManager = GameManagerGO.GetComponent<GameManager>();
         coinposition = GameManager.CoinLocationsg;
+        
     }
 	
 	// Update is called once per frame
@@ -57,8 +59,9 @@ public class Player : MonoBehaviour {
         rightprediction = Mathf.Abs((position.x + halfradius + arena.borderwidth) + speed);
         upprediction = Mathf.Abs((position.y + halfradius + arena.borderwidth) + speed);
         downprediction = Mathf.Abs((position.y - halfradius - arena.borderwidth) - speed);
+        ObstaclesArray = GameObject.FindObjectsOfType<Obstacle>();
         //check to see if we are getting to left edge, if we are far away enough, add in the left direction
-        if(leftbool && leftprediction < halfwidth)
+        if (leftbool && leftprediction < halfwidth)
         {
             movementvector.x -= speed;
         }
@@ -97,6 +100,7 @@ public class Player : MonoBehaviour {
         {
             movementvector.y -= Mathf.Abs((halfheight - arena.borderwidth - halfradius) + position.y);
         }
+        movementvector = this.ObstacleCheck(movementvector);
         this.gameObject.transform.position += movementvector;
         //see if we are colliding with a coin 
         this.Coinhit();
@@ -118,6 +122,94 @@ public class Player : MonoBehaviour {
                 GameManager.CoinNumber++;
             }
         }
+    }
+    //check to see if the player is crashing into an obstacle
+    public Vector3 ObstacleCheck(Vector3 Movement)
+    {
+        bool closeEnough;
+        for(int x =0;x<ObstaclesArray.Length;x++)
+        {
+            closeEnough = false;
+            Obstacle Obstacletest = ObstaclesArray[x];
+            //find if the player is close enough to obstacle
+            Vector3 Displacement =   this.Position - Obstacletest.Location;
+            if(Displacement.x > Obstacletest.Halfwidth)
+            {
+                Displacement.x = Obstacletest.Halfwidth;
+            }
+            if(Displacement.y > Obstacletest.Halfwidth)
+            {
+                Displacement.y = Obstacletest.Halfwidth;
+            }
+            if(Displacement.x < (-1.0f * Obstacletest.Halfwidth))
+            {
+                Displacement.x = (-1.0f * Obstacletest.Halfwidth);
+            }
+            if(Displacement.y < (-1.0f * Obstacletest.Halfwidth))
+            {
+                Displacement.y = (-1.0f * Obstacletest.Halfwidth);
+            }
+            Vector3 newloc = Obstacletest.Location + Displacement;
+            float distance = Vector3.Distance(newloc, this.Position);
+            if(distance<=Obstacletest.Halfwidth)
+            {
+                closeEnough = true;
+            }
+            bool ontop = false;
+            bool onbottom = false;
+            bool onleft = false;
+            bool onright = false;
+            if((this.position.x+this.radius)>(Obstacletest.Location.x-Obstacletest.Halfwidth+.05) && (this.position.x-this.radius)<(Obstacletest.Location.x+Obstacletest.Halfwidth-.05)&&this.position.y>Obstacletest.Location.y)
+            {
+                ontop = true;
+            }
+            if ((this.position.x+this.radius) > (Obstacletest.Location.x - Obstacletest.Halfwidth + .05) && (this.position.x-this.radius) < (Obstacletest.Location.x + Obstacletest.Halfwidth-.05) && this.position.y < Obstacletest.Location.y)
+            {
+                onbottom = true;
+            }
+            if((this.position.y+this.radius)>(Obstacletest.Location.y-Obstacletest.Halfwidth+.05)&&((this.position.y-this.radius)<(Obstacletest.Location.y+Obstacletest.Halfwidth-.05))&&this.position.x>Obstacletest.Location.x)
+            {
+                onright = true;
+            }
+            if ((this.position.y + this.radius) > (Obstacletest.Location.y - Obstacletest.Halfwidth+.05) && ((this.position.y - this.radius) < (Obstacletest.Location.y + Obstacletest.Halfwidth-.05)) && this.position.x < Obstacletest.Location.x)
+            {
+                onleft = true;
+            }
+            float ypostestbottom = Mathf.Abs(Obstacletest.Location.y - (this.Position.y + this.Radius));
+            float xpostestleft = Mathf.Abs((Obstacletest.Location.x) - (this.Position.x + this.Radius));
+            float xpostestright = Mathf.Abs((this.Position.x - this.Radius) - (Obstacletest.Location.x));
+            //see if it is crashing into the top 
+            if (ontop == true&&downbool==true&&closeEnough==true)
+            {
+                Movement.y = 0; 
+            }
+            //see if it is crashing into the bottom
+            if (onbottom==true&&upbool == true && closeEnough == true)
+            {
+               
+                
+                    Movement.y = 0;
+                
+            }
+            //see if it is crashing into the left 
+            
+            if (onleft == true && rightbool == true && closeEnough == true)
+            {
+                
+                    Movement.x = 0;
+                
+            }
+            //see if it is crashing into the right 
+            
+            if (onright == true && leftbool == true && closeEnough == true)
+            {
+                
+                    Movement.x = 0;
+                
+            }
+            
+        }
+        return Movement;
     }
 
     public float Radius
